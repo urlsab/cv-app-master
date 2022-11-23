aaf86d40e67dc9507c11c089f0edb743e8bed341
yair sabag <yairsabag213@gmail.com>
2022-11-23 15:56:22 +0200

    fix routing of react router dom v6

--- src/App.js
+++ src/App.js
@@ -4,8 +4,13 @@ import { Routes ,Route } from 'react-router-dom';
 
 import Navbar from './Components/Navbar/Navbar';
 import AllResumes from './Components/AllResumes/AllResumes';
-import PdfResume from './Components/PdfResume/PdfResume';
+// import PdfResume from './Components/PdfResume/PdfResume';
 import InputsForm from './Components/InputsForm/InputsForm';
+import LoginApp from './Components/LoginApp/LoginApp';
+import RegisterApp from './Components/RegisterApp/RegisterApp';
+import Reset from './Components/Reset/Reset';
+import Dashboard from './Components/Dashboard/Dashboard';
+import NotUser from './Components/NotUser/NotUser';
 
 // import EditToFirestore from './Components/EditToFireStore/EditToFirestore';
 
@@ -14,12 +19,16 @@ const App = () => {
       <React.Fragment>
         <Navbar/>
         <Routes>
-          <Route path='/' element={<h1>Wellcome !</h1>} exact="true" />
+          <Route path='/' element={<h1> Wellcome ! </h1>} exact="true" />
+          <Route path='/register' element={<RegisterApp/>} exact="true" />
+          <Route path='/login' element={<LoginApp/>} exact="true" />
+          <Route path='/reset' element={<Reset/>} exact="true" />
+          <Route path='/notUser' element={<NotUser/>} exact="true" />
           <Route path='/postInputs' element={<InputsForm/>} exact="true" />
           <Route path='/allResumes' element={<AllResumes/> } exact="true" />
-          <Route path='/createPdfResume' element={<PdfResume/> } exact="true" />
+          <Route path='/dashboard' element={<Dashboard/> } exact="true" />
+          {/* <Route path='/createPdfResume' element={<PdfResume/> } exact="true" /> */}
           {/*maybe add: <Route path='/allResumes/:id' element={<AllResumes/> } exact="true" /> */}
-          {/* <Route path='/inputForm' element={<InputsForm/> } exact="true" /> */}
         </Routes>
         
         {/* <EditToFirestore/> */}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, useRouteError } from 'react-router-dom';
 import './index.css';
 import App from './App';
+// import Dashboard from './Components/Dashboard/Dashboard';
 import reportWebVitals from './reportWebVitals';
+// import RegisterApp from './Components/RegisterApp/RegisterApp';
+// import LoginApp from './Components/LoginApp/LoginApp';
+// import InputsForm from './Components/InputsForm/InputsForm';
+// import AllResumes from './Components/AllResumes/AllResumes';
+
+const router = createBrowserRouter([
+  {
+    path: "*",
+    element: <App/>
+    //,
+    // children: [
+    //   {
+    //     path: "register",
+    //     element: <RegisterApp />,
+    //   },
+    //   {
+    //     path: "login",
+    //     element: <LoginApp />,
+    //   },
+    //   {
+    //     path: "Dashboard",
+    //     element: <Dashboard />,
+    //   },
+    //   {
+    //     path: "postInputs",
+    //     element: <InputsForm />,
+    //   },
+    //   {
+    //     path: "allResumes",
+    //     element: <AllResumes/>,
+    //   },
+      
+    // ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
