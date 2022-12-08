05f6bdd487ff1dfe0106531e470472320f8c53a7
yair sabag <yairsabag213@gmail.com>
2022-12-08 14:14:42 +0200

    sign up + svae password inside submit

--- src/Components/RegisterApp/RegisterApp.js
+++ src/Components/RegisterApp/RegisterApp.js
@@ -1,76 +1,75 @@
-import React, { useEffect, useState } from "react";
-import { useAuthState } from "react-firebase-hooks/auth";
-import {  createUserWithEmailAndPassword  } from 'firebase/auth';
-import { Link, useNavigate, NavLink } from "react-router-dom";
-import { auth, signInWithGoogle, registerWithEmailAndPassword } from "../../../src/firestoreConfig/firestoreConfig";
 import "./RegisterApp.css";
+
+import React, { useState } from "react";
+import { useNavigate, NavLink } from "react-router-dom";
+
+import { collection, doc, setDoc } from "firebase/firestore";
+import {  createUserWithEmailAndPassword  } from 'firebase/auth';
+
+import { auth } from "../../firestoreConfig/firestoreConfig"
+import { firestoreDB } from "../../firestoreConfig/firestoreConfig";
+
+import { initialPassword } from "../../utils/passwordsObject";
 
 const RegisterApp = () => {
 
-  const navigate = useNavigate();
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [emailAdd, setEmailAdd] = useState('');
+    const [rePassword, setRePassword] = useState('');
 
-    const [firstName, setFirstName] = useState('')
-    const [lastName, setLastName] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
 
-  // const [email, setEmail] = useState("");
-  // const [password, setPassword] = useState("");
-  // const [name, setName] = useState("");
-  // const [user, loading, error] = useAuthState(auth);
-  // const history = useNavigate();
+  const onSubmitHandler = async (e) => {
 
-
-  const onSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     
-    await createUserWithEmailAndPassword(auth, email, password)
+    await createUserWithEmailAndPassword(auth, emailAdd, rePassword)
+
+    // hash of "hesed2emet1" = 'UkVEQUNURUQ=' , UkVEQUNURUQ=
       .then((userCredential) => {
-          // Signed in 
-          const user = userCredential.user;
-          console.log(user);
-          navigate("/login")
-          // ...
+          const userData = userCredential.user;
+          console.log(auth);
+          console.log(userData);
+          console.log(`the user password is: ${rePassword}`);
       })
       .catch((error) => {
           const errorCode = error.code;
           const errorMessage = error.message;
           console.log(errorCode, errorMessage);
-          // ..
       });
-
-  
-  }
-
-  // const register = () => {
-  //   if (!name) alert("Please enter name");
-  //   registerWithEmailAndPassword(name, email, password);
-  // };
-  // useEffect(() => {
-  //   if (loading) return;
-  //   if (user) history("/dashboard");
-  // }, [user, loading, error]);
+      // save user password in new collection
+      initialPassword.objectPassword.thePassword = rePassword;
+      const usersCollection = collection(firestoreDB, `${emailAdd}`);
+      setDoc(doc(usersCollection, "userPassword"), initialPassword.objectPassword)
+        .then(() => {
+          navigate("/");
+      }).catch(error => {
+          console.log(error);
+  })
+}
 
   return (
 
     <>
-    <main >        
+    <main className="registerStyle" >        
         <section>
-            <div className="flex h-screen items-center justify-center px-4 sm:px-6 lg:px-8">
-                <div className="w-full max-w-md space-y-8">
-                    <div>
-                        
 
-                        <h2 className="text-white text-center text-base  tracking-tight text-gray-900">
+            <div>
+
+                <div>
+
+                    <div>
+                        <h2>
                             Are you new? Sign up today
                         </h2>                        
                     </div>
 
-                    
-                    <form onSubmit={onSubmit} className="mt-8 space-y-6" >                    
-                        <div className=" space-y-6 rounded-md shadow-sm">
+                    <form onSubmit={onSubmitHandler}>                   
+                        <div>
+
                             <div>
-                                <label htmlFor="email-address" className="sr-only">
+                                <label htmlFor="email-address">
                                     First name
                                 </label>
                                 <input
@@ -80,13 +79,12 @@ const RegisterApp = () => {
                                     name="firstname"
                                     type="text"                                    
                                     required                                
-                                    className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                                     placeholder="First name"                                   
                                 />
                             </div>
 
                             <div>
-                                <label htmlFor="email-address" className="sr-only">
+                                <label htmlFor="email-address">
                                     Last name
                                 </label>
                                 <input
@@ -96,7 +94,6 @@ const RegisterApp = () => {
                                     required
                                     type="text"
                                     name="lastname"                                                                       
-                                    className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                                     placeholder="Last name"                                    
                                 />
                             </div>
@@ -108,10 +105,9 @@ const RegisterApp = () => {
                                 <input
                                     type="email"
                                     label="Email address"
-                                    value={email}
-                                    onChange={(e) => setEmail(e.target.value)}                                    
+                                    value={emailAdd}
+                                    onChange={(e) => setEmailAdd(e.target.value)}                                    
                                     required
-                                    className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                                     placeholder="Email address"                                
                                 />
                             </div>
@@ -123,77 +119,28 @@ const RegisterApp = () => {
                                 <input
                                     type="password"
                                     label="Create password"
-                                    value={password}
-                                    onChange={(e) => setPassword(e.target.value)}                                    
+                                    value={rePassword}
+                                    onChange={(e) => setRePassword(e.target.value)}                                    
                                     required
-                                    className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                                     placeholder="Password"                                
                                 />
                             </div>
                         </div>                        
-
                         <div>
-                            <button
-                                type="submit"                                                               
-                                className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                            >   
-                                Sign up                                                             
-                            </button>
-                        </div>
-                                             
+                            <button type="submit"> Sign up </button>
+                        </div>                        
                     </form>
-                   
-
                     <p className="text-sm text-white text-center">
                         Already have an account?{' '}
-                        <NavLink to="/login" className="underline text-tertiary">
-                            Sign in
-                        </NavLink>
+                        <NavLink to="/">Sign in</NavLink>
                     </p>
-                    
                 </div>
             </div>
         </section>
-    </main>
+      </main>
     </>
-
-    // <div className="register">
-    //   <div className="register__container">
-    //     <input
-    //       type="text"
-    //       className="register__textBox"
-    //       value={name}
-    //       onChange={(e) => setName(e.target.value)}
-    //       placeholder="Full Name"
-    //     />
-    //     <input
-    //       type="text"
-    //       className="register__textBox"
-    //       value={email}
-    //       onChange={(e) => setEmail(e.target.value)}
-    //       placeholder="E-mail Address"
-    //     />
-    //     <input
-    //       type="password"
-    //       className="register__textBox"
-    //       value={password}
-    //       onChange={(e) => setPassword(e.target.value)}
-    //       placeholder="Password"
-    //     />
-    //     <button className="register__btn" onClick={register}>
-    //       Register
-    //     </button>
-    //     <button
-    //       className="register__btn register__google"
-    //       onClick={signInWithGoogle}
-    //     >
-    //       Register with Google
-    //     </button>
-    //     <div>
-    //       Already have an account? <Link to="/login">Login now</Link>
-    //     </div>
-    //   </div>
-    // </div>
   );
+
 }
+
 export default RegisterApp;
