55e5ef23d5dec6ea9855978f611f6c638e991ff1
yair sabag <yairsabag213@gmail.com>
2022-12-12 15:46:31 +0200

    almost set remind password func

--- src/Components/ResetPassword/ResetPassword.js
+++ src/Components/ResetPassword/ResetPassword.js
@@ -1,53 +1,64 @@
 import './ResetPassword.css'
 
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
-import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { useNavigate ,Link } from "react-router-dom";
+
+import { query, where } from "firebase/firestore";
 
 import emailjs from '@emailjs/browser';
 import randomString from 'randomstring';
 import { useAuthState } from "react-firebase-hooks/auth";
-//,UpdatePasswordHook,useUpdatePassword,useUpdateProfile
+import {  createUserWithEmailAndPassword, signInWithEmailLink  } from 'firebase/auth';
 
-import { auth } from "../../firestoreConfig/firestoreConfig";
+import { collection } from "firebase/firestore";
+
+// import { initialPassword } from "../../utils/passwordsObject";
+
+// import { documentId, setDoc } from "firebase/firestore";
+import { firestoreDB } from "../../firestoreConfig/firestoreConfig";
+import { auth } from "../../../src/firestoreConfig/firestoreConfig";
+// import { addDoc, collection } from "firebase/firestore";
+import { doc, updateDoc, getDocs } from 'firebase/firestore';
+import { initialPassword } from '../../utils/passwordsObject';
+
 
 const Reset = () => {
 
-  const navigate = useNavigate();
-  const theNewPassword = randomString.generate(9);
-
   const form = useRef();
+
+  const navigate = useNavigate();
+
+  const [user] = useAuthState(auth);
   const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-
-  // maybe delete setNewPassword
+  const [emailAdd, setEmail] = useState('');
   const [prevPassword, setNewPassword] = useState('');
-  const [user, loading, error] = useAuthState(auth);
-
-// const updatePassword = useUpdatePassword(auth, prevPassword);
-
+  
   const sendEmail = (e) => {
     e.preventDefault();
-
+    // const q = collection(firestoreDB, `${user.email}`);
+    console.log(`your password is: ${user.displayName}`);
     emailjs.sendForm(
       process.env.REACT_APP_SERVICE_ID, 
       process.env.REACT_APP_TEMPLATE_ID, 
       form.current, 
       process.env.REACT_APP_PUBLIC_KEY, {
         user_name: name,
-        message: prevPassword,
-        user_email: email
+        message: user.displayName,
+        user_email: emailAdd
     })
       .then((result) => {
           console.log(result.text);
-          // console.log(theNewPassword);
-          navigate("/gotNewPassword");
+          console.log(prevPassword);
+          console.log(name);
+          console.log(result.status);
+          console.log(result);
+          // navigate("/gotNewPassword");
       }, (error) => {
           console.log(error.text);
       });
   };
-   
+ 
   return (
     <div className="resetStyle">
     <form ref={form} onSubmit={sendEmail} >
@@ -63,28 +74,31 @@ const Reset = () => {
         name="user_email" 
         placeholder='email' 
         required 
-        value={email}
+        value={emailAdd}
         onChange={(e)=>setEmail(e.target.value)} />
       <input 
         type="text" 
-        name="message" 
+        // name="message" 
         placeholder='your message' 
         required
-        disabled
+        // disabled
         // hidden
 
         // fix: for any type - we render new rand password - should be only after submit
-        value={theNewPassword}
-        // onChange={(e)=>setNewPassword(theNewPassword)} 
+        value={prevPassword}
+        onChange={(e)=> setNewPassword(e.target.value)}
         />
       <input 
         type="submit" 
         value="Send new password to my email" />
     </form>
+    {/* <button> match email path to user</button> */}
     <div>
           Don't have an account? <Link to="/">Register now </Link>
     </div>
     </div>
   );
+
 }
+
 export default Reset;
