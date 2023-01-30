208c5821acec5f8fd63d017cf7531b26377e8329
yair sabag <yairsabag213@gmail.com>
2023-01-30 13:56:00 +0200

    style & animate login form

--- src/Components/LoginApp/LoginApp.js
+++ src/Components/LoginApp/LoginApp.js
@@ -1,5 +1,8 @@
 import "./LoginApp.css";
 
+import TextField from '@mui/material/TextField';
+import { Button } from "@mui/material";
+
 import React, { useState } from "react";
 import { useNavigate, NavLink } from "react-router-dom";
 
@@ -8,128 +11,113 @@ import { signInWithEmailAndPassword } from 'firebase/auth';
 
 import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 
+import Fade from 'react-reveal/Fade';
+
 const LoginApp = () => {
 
   // const [user, loading, error] = useAuthState(auth);
   
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-  //   if (loading) {
-  //     // maybe trigger a loading screen
-
-  const googleSignIn = () => {
-    const googleAuth = new GoogleAuthProvider();
-    return signInWithPopup(auth, googleAuth);
-  }
-
-  const handleGoogleSignIn = async (e) => {
-    e.preventDefault();
-    try {
-      await googleSignIn();
-      navigate("/dashboard");
-    } catch (error) {
-      console.log(error.message);
+    const googleSignIn = () => {
+        const googleAuth = new GoogleAuthProvider();
+        return signInWithPopup(auth, googleAuth);
     }
-  };
 
+    const handleGoogleSignIn = async (e) => {
+        e.preventDefault();
+        try {
+            await googleSignIn();
+            navigate("/dashboard");
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
 
-  const onLogin = (e) => {
-    e.preventDefault();
-    signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-        const user = userCredential.user;
-        console.log(user);
-        console.log(password);
-        navigate("/dashboard");
-        
-    })
-    .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(errorCode, errorMessage);
-        alert(errorCode);
-    });
-}
+    const onLogin = (e) => {
+        e.preventDefault();
+        signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            console.log(user);
+            console.log(password);
+            navigate("/dashboard");
+            
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorCode, errorMessage);
+            alert(errorCode);
+        });
+    }
 
-  return (
+    return (
 
-    <>
-            <main className="loginStyle" >        
-                <section>
-                    <div className="flex h-screen items-center justify-center px-4 sm:px-6 lg:px-8">
-                        <div className="w-full max-w-md space-y-8">
-                            <div>
-                                <h1> wellcome to cv builder website ! </h1>
+        <>
 
-                                <h2 className="text-white text-center text-base  tracking-tight text-gray-900">
-                                    log in here
-                                </h2>                        
-                            </div>
-                            
-                            <form className="mt-8 space-y-6" >                            
-                                <div className=" space-y-6 rounded-md shadow-sm">
-                                                                            
-                                    
-                                    <div>
-                                        <label htmlFor="email-address" className="sr-only">
-                                        Email address
-                                        </label>
-                                        <input
-                                            id="email-address"
-                                            name="email"
-                                            type="email"                                    
-                                            required                                            
-                                            className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                                            placeholder="Email address"
-                                            onChange={(e)=>setEmail(e.target.value)}
-                                        />
-                                    </div>
+            <div className="mainContainer">
 
-                                    <div>
-                                        <label htmlFor="password" className="sr-only">
-                                            Password
-                                        </label>
-                                        <input
-                                            id="password"
-                                            name="password"
-                                            type="password"                                    
-                                            required                                            
-                                            className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                                            placeholder="Password"
-                                            onChange={(e)=>setPassword(e.target.value)}
-                                        />
-                                    </div>
-                                </div>                        
-
-                                <div style={{marginTop: "20px"}}>
-                                    <button                                     
-                                        onClick={onLogin}
-                                        className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                                    >      
-                                        Login                                                                  
-                                    </button>
-                                </div>
-
-                                <button onClick={handleGoogleSignIn} style={{marginTop: "20px"}}> Sign in with google </button>
-                                
-                            </form>
-                        
-                            <p className="text-sm text-white text-center">
-                                No account yet?{' '}
-                                <NavLink to="/register" className="underline text-tertiary">
-                                    Sign up
-                                </NavLink>
-                            </p>
-                            
+                <Fade top delay={300}>
+                                      
+                    <form className="loginFormContainer"> 
+                                            
+                        <div className="textFieldStyle">                                        
+                            <TextField
+                                // className="inputStyle"
+                                id="email-address"
+                                name="email"
+                                type="email" 
+                                label="Email Address"                                   
+                                required                                            
+                                placeholder="Email address"
+                                onChange={(e)=>setEmail(e.target.value)}
+                            />
                         </div>
-                    </div>
-                </section>
-            </main>
+
+                        <div className="textFieldStyle">            
+                            <TextField
+                                id="password"
+                                name="password"
+                                type="password"
+                                label="Password"                                    
+                                required                                            
+                                placeholder="Password"
+                                onChange={(e)=>setPassword(e.target.value)}
+                            />
+                        </div>
+
+                        <div className="textFieldStyle">
+                            <Button variant="contained" color="info" onClick={onLogin}> Login By Password </Button>
+                        </div>
+
+                        <div className="textFieldStyle">
+                            <Button variant="contained" color="error" onClick={handleGoogleSignIn}> Signin with google </Button>
+                        </div>         
+                        
+                        <div className="textFieldStyle">
+                                <NavLink to="/register" > Create New account</NavLink>
+                        </div>
+                        
+                    </form>
+
+                </Fade>
+
+                <div className="textContainer">
+                    <Fade left delay={1200} >  <h1 className="textStyle"> One Simple Step</h1> </Fade>
+                    <Fade left delay={2100} >  <h1 className="textStyle"> To Start Create</h1> </Fade>
+                    <Fade left delay={3000} >  <h1 className="textStyle"> An Amazing CV</h1> </Fade>
+                </div>
+                
+            </div>
+            
         </>
    
   );
+
 }
+
 export default LoginApp;
