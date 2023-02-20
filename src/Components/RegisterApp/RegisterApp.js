67ce76af6cc1b1d48a4c00268b3167bcff0991fa
yair sabag <yairsabag213@gmail.com>
2023-02-20 15:53:46 +0200

    fix 3 funcs inside register submit

--- src/Components/RegisterApp/RegisterApp.js
+++ src/Components/RegisterApp/RegisterApp.js
@@ -3,7 +3,7 @@ import "./RegisterApp.css";
 import React, { useState, useRef } from "react";
 import { useNavigate, NavLink } from "react-router-dom";
 
-import { collection, doc, setDoc } from "firebase/firestore";
+import { collection, doc, setDoc, addDoc } from "firebase/firestore";
 import {  createUserWithEmailAndPassword  } from 'firebase/auth';
 
 import { auth } from "../../firestoreConfig/firestoreConfig"
@@ -15,6 +15,7 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import TextField from '@mui/material/TextField';
 import { Button } from "@mui/material";
 
+import LockOpenIcon from '@mui/icons-material/LockOpen';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import AssignmentTurnedInIcon from '@mui/icons-material/AssignmentTurnedIn';
 import LoginIcon from '@mui/icons-material/Login';
@@ -40,62 +41,121 @@ const RegisterApp = () => {
         navigate("/");
     }
 
+    // const sendToEmail = (e) => {
+
+    //     e.preventDefault();
+
+    //     emailjs.send(
+    //         process.env.REACT_APP_SERVICE_ID,
+    //         process.env.REACT_APP_TEMPLATE_ID, {
+    //             user_name: firstName,
+    //             message: rePassword,
+    //             user_email: emailAdd
+    //         },
+    //         process.env.REACT_APP_PUBLIC_KEY
+
+    //         // process.env.REACT_APP_SERVICE_ID, 
+    //         // process.env.REACT_APP_TEMPLATE_ID, 
+    //         // form.current, 
+    //         // 'nTckRns0_Iv6mZOUS'
+    //         // process.env.REACT_APP_PUBLIC_KEY
+    //     //     , {
+    //     //     user_name: firstName,
+    //     //     message: rePassword,
+    //     //     user_email: emailAdd
+    //     // }
+    //     )
+
+    //     .then((result) => {
+    //         console.log(`email js send the password ${rePassword} to ${emailAdd}  `)
+    //         console.log(result.text);
+    //         console.log(result.status);
+    //         console.log(result);
+    //         navigate("/");
+    //     })
+
+    //     .catch((error) => {
+    //         console.log(error.text);
+    //         console.log("error from emailjs function")
+    //     });
+    // }
+
+    // add await key word = function will run any req one after one
     const onSubmitHandler = async (e) => {
+
         e.preventDefault();
+
         await createUserWithEmailAndPassword(auth, emailAdd, rePassword)
+
         // hash of "hesed2emet1" = 'UkVEQUNURUQ=' , UkVEQUNURUQ=
         .then((userCredential) => {
             let userData = userCredential.user;
             userData.displayName = rePassword;
+            userData.phoneNumber = firstName;
+            
             console.log(auth);
             console.log(userData)
             console.log(`the user password is: ${rePassword} `);
-        //   console.log(`displayName:${disName}`);
-      })
+            console.log(`displayName:${userData.displayName}`);
+            console.log(`phoneNumber:${userData.phoneNumber}`);
+        })
 
-      .catch((error) => {
+        .catch((error) => {
             const errorCode = error.code;
             const errorMessage = error.message;
             console.log(errorCode, errorMessage);
-      })
+            console.log("error from createUserWithEmailAndPassword function")
+        })
 
       // save user password in new collection
-      initialPassword.objectPassword.thePassword = rePassword;
+      
+    //   console.log(rePassword);
+    //   initialPassword.objectPassword.thePassword = rePassword;
+    
+    //   e.preventDefault();
       const usersCollection = collection(firestoreDB, `${emailAdd}`);
-      setDoc(doc(usersCollection, "userPassword"), initialPassword.objectPassword)
+      
+      await setDoc(doc(usersCollection, "firstData"), {thePassword: rePassword, userName: firstName})
         .then(() => {
-            console.log("set password in doc successfully")
+            // console.log(initialPassword.objectPassword.thePassword);
+            console.log("set password as a collection successfully");
+            // navigate("/");
         })
 
         .catch(error => {
             console.log(error);
-        });
-        console.log(`your password is: ${rePassword} `);
-        emailjs.sendForm(
+            console.log("error from set password as a collection function")
+        })
+        
+        emailjs.send(
             process.env.REACT_APP_SERVICE_ID, 
             process.env.REACT_APP_TEMPLATE_ID, 
-            form.current, 
-            process.env.REACT_APP_PUBLIC_KEY, {
+            {
             user_name: firstName,
             message: rePassword,
             user_email: emailAdd
-        })
+            },
+            process.env.REACT_APP_PUBLIC_KEY,
+        )
+
         .then((result) => {
+            console.log(`email js send the password ${rePassword} to ${emailAdd}  `)
             console.log(result.text);
             console.log(result.status);
             console.log(result);
-            navigate("/")
+            navigate("/");
         })
 
-        .catch ((error) => {
+        .catch((error) => {
             console.log(error.text);
+            console.log("error from emailjs function")
         });
         
     }
 
     return (
 
-        <>
+        // <>
     
             <div className="mainContainer">
 
@@ -103,12 +163,13 @@ const RegisterApp = () => {
                 
                     <Fade left dalay={300}>
                        
-                        <form className="loginFormContainer" onSubmit={onSubmitHandler} ref={form}>                    
+                        <form ref={form} onSubmit={onSubmitHandler}  className="loginFormContainer">                    
 
                             <TextField                              
                                 name="user_name"
                                 type="text"                                    
                                 required 
+                                // placeholder="Full name"
                                 // all fields stretch to this width
                                 sx={{width:"280px"}}
                                 InputProps={{startAdornment: (
@@ -126,6 +187,7 @@ const RegisterApp = () => {
                                 label="Email"
                                 name="user_email"
                                 required  
+                                // placeholder="Full name"
                                 InputProps={{startAdornment: (
                                     <InputAdornment position="start">
                                         <EmailIcon />
@@ -142,7 +204,7 @@ const RegisterApp = () => {
                                 required  
                                 InputProps={{startAdornment: (
                                     <InputAdornment position="start">
-                                        <PasswordIcon />
+                                        <LockOpenIcon />
                                     </InputAdornment>
                                 )}}   
                                 value={rePassword}
@@ -150,8 +212,9 @@ const RegisterApp = () => {
                             />
                                 
                             <Button startIcon={<AssignmentTurnedInIcon/>} size="large" color="success" variant="contained" type="submit"> Sign up </Button>
-
+                            
                             <Button onClick={navigateToSignIn} size="large" startIcon={<LoginIcon/>}  variant="outlined" color="primary"> login </Button>
+                            {/* <button onClick={sendToEmail}>send password to email</button> */}
                             {/* <Button> Already have an account? <NavLink to="/">Sign in</NavLink> </Button>  */}
 
                         </form> 
@@ -162,13 +225,13 @@ const RegisterApp = () => {
 
                 <div className="textContainer">
                     <Fade bottom delay={1200}> <h1> <b className="textStyle"> SIGN UP</b> </h1> </Fade>
-                    <Fade bottom delay={2100}> <h1> <b className="textStyle"> FOR </b> 🆓 </h1> </Fade>
-                    <Fade bottom delay={3000}> <h1> <b className="textStyle">CREATION</b>  </h1> </Fade>
+                    <Fade bottom delay={2100}> <h1> <b className="textStyle"> FOR FREE </b> </h1> </Fade>
+                    <Fade bottom delay={3000}> <h1> <b className="textStyle">CREATION</b> 🎨 </h1> </Fade>
                 </div>
 
             </div>
              
-        </>
+        // </>
 
     );
 
