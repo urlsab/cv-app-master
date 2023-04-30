14c6d2d3afc6e26581171c0253f44f51d04ef2e3
yair sabag <yairsabag213@gmail.com>
2023-04-30 14:50:14 +0300

    fix text box width for hovering

--- src/Components/Todo/Todo.js
+++ src/Components/Todo/Todo.js
@@ -4,6 +4,22 @@ import './Todo.css';
 
 const Todo = () => {
 
+
+  const [hoveredCart, setHoveredCart] = useState(-1);
+
+  const showCartHandler = (i) =>{
+    
+    setHoveredCart(i);
+    setDisplay('displayed');
+}
+
+const hideCartHandler= () => {
+      
+      setHoveredCart(-1);
+      setDisplay('notdisplayed');
+}
+
+
   const [display, setDisplay] = useState('notdisplayed');
   const [inputList, setInputList] = useState([{ firstName: '' }]);
 
@@ -14,9 +30,7 @@ const Todo = () => {
 
   const hideButton = (e) => {
     e.preventDefault();
-
-    
-      setDisplay('notdisplayed')
+    setDisplay('notdisplayed');
     
 
     // setDisplay('notdisplayed')
@@ -48,18 +62,20 @@ const Todo = () => {
     <div>
       {inputList.map((x, i) => {
         return (
-          <div className="box" onMouseEnter={e => showButton(e)}
-          onMouseLeave={e => hideButton(e)}>
-            <input
+          <div 
+            className="box" 
             
-            
-              name="firstName"
-              placeholder=" "
+            >
+          <input
+            onMouseEnter={e => showCartHandler(e)}
+            onMouseLeave={e => hideCartHandler(e)}
+            name="firstName"
+            placeholder=" "
               
-              value={x.firstName}
-              onChange={(e) => handleInputChange(e, i)}
-            />
-            <div  className="btn-box">
+            value={x.firstName}
+            onChange={(e) => handleInputChange(e, i)}
+          />
+            <div className="btn-box">
               {inputList.length !== 1 && (
                 <button className={display} onClick={() => handleRemoveClick(i)}>
                   Remove
@@ -76,88 +92,5 @@ const Todo = () => {
     </div>
   );
 }
-
-// export default AddRemoveInputField
-
-// import './Todo.css'
-
-// import { Button, Card, Form } from 'react-bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-
-// import { useState } from "react";
-
-
-// const Todo = ({ todo, index, markTodo, removeTodo, addTodo }) => {
-
-//   const obj = [{text: "This is a sampe todo",isDone: false}];
-
-//   const [value, setValue] = useState("");
-//   const [todos, setTodos] = useState(obj)
-    
-
-//   const handleSubmit = e => {
-//     e.preventDefault();
-//     if (!value) return;
-//     addTodo(value);
-//     setValue("");
-//   };
-
-//   const handleAddTodo = text => {
-//     const newTodos = [...todos, { text }];
-//     setTodos(newTodos);
-//   };
-
-//   const handleMarkTodo = index => {
-//     const newTodos = [...todos];
-//     newTodos[index].isDone= true;
-//     setTodos(newTodos);
-//   };
-
-//   const handleRemoveTodo = index => {
-//     const newTodos = [...todos];
-//     newTodos.splice(index, 1);
-//     setTodos(newTodos);
-//   };
-
-//   return (
-//     <div className="todo">
-//       <span style={{ textDecoration: todo.isDone ? "line-through" : "" }}>{todo.text}</span>
-//       <div>
-
-//       <h1 className="text-center mb-4">Todo List</h1>
-//         <Todo addTodo={handleAddTodo} />
-//         <div>
-//           {todos.map((todo, index) => (
-//             <Card>
-//               <Card.Body>
-//                 <Todo
-//                 key={index}
-//                 index={index}
-//                 todo={todo}
-//                 markTodo={markTodo}
-//                 removeTodo={removeTodo}
-//                 />
-//               </Card.Body>
-//             </Card>
-//           ))}
-//         </div>
-
-//         <Form onSubmit={handleSubmit}> 
-//       <Form.Group>
-//         <Form.Label><b>Add Todo</b></Form.Label>
-//         <Form.Control type="text" className="input" value={value} onChange={e => setValue(e.target.value)} placeholder="Add new todo" />
-//       </Form.Group>
-//       <Button variant="primary mb-3" type="submit">
-//         Submit
-//       </Button>
-//     </Form>
-
-//         <Button variant="outline-success" onClick={() => handleMarkTodo(index)}>✓</Button>{' '}
-//         <Button variant="outline-danger" onClick={() => handleRemoveTodo(index)}>✕</Button>
-//       </div>
-//     </div>
-//   );
-// }
-
 
 export default Todo;
