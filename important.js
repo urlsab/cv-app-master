f4b74930d4fde4c5ec4795c8e767392718027430
yair sabag <yairsabag213@gmail.com>
2024-02-12 15:35:17 +0200

    comment bug scripts

---
+++ important.js
@@ -0,0 +1,38 @@
+// <!DOCTYPE html>
+//     <html lang="en">
+//         <head>
+//             <meta charset="UTF-8">
+//             <meta name="viewport" content="width=device-width,initial-scale=1">
+//             <title>CV-APP-MASTER</title>
+//             <style>@import url('https://fonts.googleapis.com/css2?family=Itim&family=Open+Sans&family=Poppins:wght@200&family=Cormorant+Garamond&family=Poppins:wght@200&display=swap&family=EB+Garamond&family=Exo+2&family=Lato&family=Lora&family=Mandali&family=Merriweather&family=Montserrat&family=Rubik&family=Spectral&display=swap');</style>
+//             <meta name="theme-color" content="#000000"/>
+//             <meta name="description" content="Web site created using create-react-app"/>
+//             <link rel="stylesheet" href="https://kendo.cdn.telerik.com/themes/6.6.0/default/default-ocean-blue.css">
+//             <link rel="manifest" href="manifest.json"/>
+
+//             <script href="static/css/main.a6d7cea6.css" rel="stylesheet"></script>
+//             <script href="static/css/main.a6d7cea6.css.map" rel="stylesheet"></script>
+
+//             <script href="static/js/729.15c0b537.chunk.js" rel="stylesheet"></script>
+//             <script href="static/js/729.15c0b537.chunk.js.map" rel="stylesheet"></script>
+
+//             <script href="static/js/main.83d352ef.js" rel="stylesheet"></script>
+//             <script href="static/js/main.83d352ef.js.map" rel="stylesheet"></script>
+
+//             <!-- <script href="static/js/main.d0a992f9.js" rel="stylesheet"></script> -->
+//             <!-- <script href="static/js/main.d0a992f9.js.map" rel="stylesheet"></script> -->
+//             <!-- <script defer="defer" src="static/js/main.d0a992f9.js"></script> -->
+//             <!-- <script href="static/js/main.f27c27cd.js" rel="stylesheet"></script> -->
+
+//         </head>
+//             <body>
+//                 <div id="root"></div>
+//             </body>
+//     </html>
+
+//     <!-- 1. set "hosting": {
+//     "public": "build", in firebase.json == this file will render
+
+
+// 2. make paths correct
+// 3. be sure files exist in build>static directory
