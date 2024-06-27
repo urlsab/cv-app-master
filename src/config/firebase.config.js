d4d7e4bbf3469ad4cd5be2dde51daac0335e7a49
yair sabag <yairsabag213@gmail.com>
2024-06-27 15:18:14 +0300

    display env vars on config for test prod issues

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -2,14 +2,24 @@ import { getAuth } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 import { initializeFirestore } from "firebase/firestore";
 
-const firebaseConfig = {  
-  apiKey: `${process.env.REACT_APP_API_KEY}`,
-  authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
-  databaseURL: `${process.env.REACT_APP_DB_URL}`,
-  projectId: `${process.env.REACT_APP_PROJECT_ID}`,
-  storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
-  messagingSenderId: `${process.env.REACT_APP_SENDER_ID}`,
-  appId: `${process.env.REACT_APP_API_ID}`
+// const firebaseConfig = {  
+//   apiKey: `${process.env.REACT_APP_API_KEY}`,
+//   authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
+//   databaseURL: `${process.env.REACT_APP_DB_URL}`,
+//   projectId: `${process.env.REACT_APP_PROJECT_ID}`,
+//   storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
+//   messagingSenderId: `${process.env.REACT_APP_SENDER_ID}`,
+//   appId: `${process.env.REACT_APP_API_ID}`
+// };
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCEryolwTvi751-v2evqPUquUvlcugNys8",
+  authDomain: "resumes-builder.firebaseapp.com",
+  databaseURL: "https://resumes-builder-default-rtdb.firebaseio.com",
+  projectId: "resumes-builder",
+  storageBucket: "resumes-builder.appspot.com",
+  messagingSenderId: "235192135514",
+  appId: "1:235192135514:web:87d29f46541b32550d0d63"
 };
 
 export const app = initializeApp(firebaseConfig); 
